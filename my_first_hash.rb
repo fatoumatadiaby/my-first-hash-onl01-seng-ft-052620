700ce2f907cc1ab98f8ff00631dae621abb53b1b
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-06-03 14:54:07 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,35 +4,35 @@ end
 
 
 def shipping_manifest
-  the_manifest ={"whale bone corset" => 5, "porcelain vase" => 2,
+  the_manifest = {"whale bone corset" => 5, "porcelain vase" => 2,
 "oil painting" => 3}
  end
 
-def retrieval
-  shipping_manifest = {
-               "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
+# def retrieval
+#   shipping_manifest = {
+#               "whale bone corsets" => 5, 
+#                   "porcelain vases" => 2, 
+#                   "oil paintings" => 3
+#                 }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+#   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
-end
+# end
 
-def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
+# def adding
+#   shipping_manifest = {
+#                   "whale bone corsets" => 5, 
+#                   "porcelain vases" => 2, 
+#                   "oil paintings" => 3
+#                 }
+#   # add 2 muskets to the shipping_manifest hash below
 
 
 
-  # return the shipping_manifest hash below
+#   # add 4 gun powder to the shipping_manifest hash below
 
-end
+
+
+#   # return the shipping_manifest hash below
+
+# end
